c0905eb358b1096396124e272b9da55cb4593b77
Emmanuel Macaulay <61067933+Oba-max22@users.noreply.github.com>
2020-10-17 20:23:36 +0100

    MajorTask.php

---
+++ MajorTask.php
@@ -0,0 +1,38 @@
+<?php
+    
+    //*********Major Task ********/
+
+    $num1 = 23;
+    $num2 = 10;
+
+    //Addition
+    echo "<h1>Addition</h1>";
+    echo "<p>This program adds two variables together</p>";
+    $add = $num1 + $num2;
+    echo "$num1 + $num2 = ".$add;
+
+    //Substraction
+    echo "<h1>Substraction</h1>";
+    echo "<p>This program substracts two variables together</p>";
+    $substract = $num1 - $num2;
+    echo "$num1 - $num2 = ".$substract;
+
+    //Multiplication
+    echo "<h1>Multiplication</h1>";
+    echo "<p>This program multiplies two variables together</p>";
+    $multiply = $num1 * $num2;
+    echo "$num1 * $num2 = ".$multiply;
+
+    //Division
+    echo "<h1>Division</h1>";
+    echo "<p>This program divides two variables together</p>";
+    $divide = $num1 / $num2;
+    echo "$num1 / $num2 = ".$divide;
+
+    //Modulus
+    echo "<h1>Modulus</h1>";
+    echo "<p>This program divides two variables together and returns the remainder</p>";
+    $modulus = $num1 % $num2;
+    echo "$num1 % $num2 = ".$modulus;
+
+?>
